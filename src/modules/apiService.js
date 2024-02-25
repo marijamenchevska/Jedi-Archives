3f58d0f87f4c6de744a74723811715b2924dd915
Marija Menchevska <marija.menchevska@outlook.com>
2024-02-25 22:40:47 +0100

    corrected comment in ApiService

--- src/modules/apiService.js
+++ src/modules/apiService.js
@@ -3,7 +3,7 @@ export class ApiService {
         this.url = "https://swapi.dev/api/";
     }
 
-    // Different URL weding for searching items
+    // Different URL ending for searching items
     async getAPIdata (dataType, page, urlEnding = `/?page=${page}`) {
         const response = await fetch(`${this.url}${dataType}${urlEnding}`);
         const parsedReponse = response.json();
